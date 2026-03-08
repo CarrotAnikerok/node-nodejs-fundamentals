81199ab589530e3b4904aa7bb327b6bae9e11c11
carrot <viktoriya.korekina@simpleone.ru>
2026-03-08 23:47:56 +0300

    feat: add lineNumberer

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,40 @@
+import os from 'node:os'
+import { Transform } from 'node:stream';
+
+class TransformLines extends Transform  {
+    lineCount = 1;
+    chunkLeft = '';
+    
+
+    constructor(options) {
+        super(options);
+    }
+
+    _transform(chunk, encoding, callback) {
+        const stringChunk = this.chunkLeft + chunk.toString();
+        const strokes = stringChunk.split('\n');
+        this.chunkLeft = strokes.pop();
+
+        for (let stroke of strokes) {
+            this.push(`${this.lineCount++} | ${stroke}\n`);
+        }
+
+        callback();
+    }
+
+    _flush(callback) {
+        if (this.chunkLeft) {
+            this.push(`${this.lineCount++} | ${this.chunkLeft}\n`);
+        }
+
+        callback();
+    }
+}
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+    const transform = new TransformLines();
+
+    process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 lineNumberer();
